0a6344014ca5b4311e3d13193336e7b0855cb9b1
Ibrahim <Ibrahim@ebs.net.pk>
2024-09-11 11:59:32 +0200

    refactor: move register user into auth handler

--- webserver/src/handlers/auth_handler.rs
+++ webserver/src/handlers/auth_handler.rs
@@ -1,10 +1,19 @@
-use actix_web::{post, web, HttpResponse, Responder};
+use actix_web::{HttpResponse, post, Responder, web};
 use serde::Deserialize;
 
 use crate::{auth::jwt_auth_service::create_jwt, database::db::DbPool, services::user_service};
+use crate::handlers::auth_handler;
+use crate::models::user::PublicUser;
 
 #[derive(Deserialize)]
 pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Deserialize)]
+pub struct RegisterRequest {
+    pub username: String,
     pub email: String,
     pub password: String,
 }
@@ -18,13 +27,47 @@ pub async fn login(
     match user_service::login(&mut conn, &credentials.email, &credentials.password).await {
         Ok(user) => {
             let bearer_token = create_jwt(&user.email);
+            let public_user: PublicUser = user.into();
             HttpResponse::Ok()
                 .append_header(("Authorization", bearer_token))
-                .json(user)
+                .json(public_user)
         }
         Err(error) => {
             log::error!("Failed to login User: {:?}", error);
             HttpResponse::Unauthorized().json("Invalid username or password")
         }
     }
+}
+
+#[post("/register")]
+pub async fn register(
+    pool: web::Data<DbPool>,
+    credentials: web::Json<RegisterRequest>,
+) -> impl Responder {
+    let mut conn = pool.get().expect("Failed to get DB connection.");
+    match user_service::register_user(
+        &mut conn,
+        &credentials.username,
+        &credentials.password,
+        &credentials.email,
+    )
+    .await
+    {
+        Ok(user) => {
+            let public_user: PublicUser = user.into();
+            HttpResponse::Created().json(public_user)
+        }
+        Err(error) => {
+            log::error!("Failed to create new User: {:?}", error);
+            HttpResponse::InternalServerError().json("Error creating new User")
+        }
+    }
+}
+
+
+
+pub fn auth_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::scope("/auth")
+        .service(login))
+        .service(register);
 }
--- webserver/src/handlers/user_handler.rs
+++ webserver/src/handlers/user_handler.rs
@@ -3,34 +3,6 @@ use actix_web::{get, post, web, HttpMessage, HttpRequest, HttpResponse, Responde
 use serde::Deserialize;
 use serde_json::json;
 
-#[derive(Deserialize)]
-pub struct RegisterRequest {
-    pub username: String,
-    pub email: String,
-    pub password: String,
-}
-
-#[post("")]
-pub async fn register(
-    pool: web::Data<DbPool>,
-    credentials: web::Json<RegisterRequest>,
-) -> impl Responder {
-    let mut conn = pool.get().expect("Failed to get DB connection.");
-    match user_service::register_user(
-        &mut conn,
-        &credentials.username,
-        &credentials.password,
-        &credentials.email,
-    )
-    .await
-    {
-        Ok(user) => HttpResponse::Created().json(user),
-        Err(error) => {
-            log::error!("Failed to create new User: {:?}", error);
-            HttpResponse::InternalServerError().json("Error creating new User")
-        }
-    }
-}
 
 #[get("/info")]
 async fn me(user_sub: UserSub) -> impl Responder {
