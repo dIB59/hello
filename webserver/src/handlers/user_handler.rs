df0f0ddaf54adc5567d11d5dc0452a67f35ad812
Ibrahim <Ibrahim@ebs.net.pk>
2024-09-11 12:39:07 +0200

    refactor: move user routes

--- webserver/src/handlers/user_handler.rs
+++ webserver/src/handlers/user_handler.rs
@@ -1,10 +1,18 @@
-use crate::{auth::auth_middleware::UserSub, db::DbPool, services::user_service};
-use actix_web::{get, post, web, HttpMessage, HttpRequest, HttpResponse, Responder};
-use serde::Deserialize;
+use actix_web::{get, HttpResponse, Responder, web};
 use serde_json::json;
 
+use crate::auth::auth_middleware;
+use crate::models::user::UserSub;
 
 #[get("/info")]
 async fn me(user_sub: UserSub) -> impl Responder {
     HttpResponse::Ok().json(json!({ "user_sub": user_sub}))
+}
+
+pub fn user_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/users")
+            .wrap(auth_middleware::Auth)
+            .service(me),
+    );
 }
--- webserver/src/routes.rs
+++ webserver/src/routes.rs
@@ -1,8 +1,10 @@
 use actix_web::web;
+
+use auth_handler::auth_routes;
 use task_handler::task_routes;
+use user_handler::user_routes;
 
 use crate::{
-    auth::auth_middleware,
     chat::{chat_route, get_count},
     handlers::*,
 };
@@ -11,26 +13,9 @@ pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api/v1")
             .configure(task_routes)
-            .configure(protect)
             .configure(user_routes)
             .configure(auth_routes)
             .route("/count", web::get().to(get_count)),
     );
     cfg.service(web::scope("/ws").route("/chat", web::get().to(chat_route)));
-}
-
-fn user_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/users").service(user_handler::register));
-}
-
-fn auth_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/auth").service(auth_handler::login));
-}
-
-fn protect(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::scope("/protected")
-            .wrap(auth_middleware::Auth)
-            .service(user_handler::me),
-    );
 }
