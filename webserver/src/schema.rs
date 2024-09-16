58f96bce5756fd3c35b83ae99318adae71743992
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-09-16 08:26:25 +0200

    feat: add project table to schema

--- webserver/src/schema.rs
+++ webserver/src/schema.rs
@@ -1,6 +1,17 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    projects (id) {
+        id -> Int4,
+        user_id -> Int4,
+        #[max_length = 255]
+        title -> Varchar,
+        description -> Text,
+        created_at -> Timestamp,
+    }
+}
+
+diesel::table! {
     tasks (id) {
         id -> Int4,
         description -> Text,
@@ -14,12 +25,14 @@ diesel::table! {
         id -> Int4,
         username -> Varchar,
         password_hash -> Varchar,
+        #[max_length = 255]
         email -> Varchar,
         created_at -> Timestamp,
     }
 }
 
 diesel::allow_tables_to_appear_in_same_query!(
+    projects,
     tasks,
     users,
 );
