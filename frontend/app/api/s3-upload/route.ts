16581b3766f3095ecef2f017aeffb520b128be47
stefanpython <stefanhtml@yahoo.com>
2024-09-20 12:32:01 +0300

    Make .env variables private

--- frontend/app/api/s3-upload/route.ts
+++ frontend/app/api/s3-upload/route.ts
@@ -5,10 +5,10 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
  * Initialize S3 client with credentials from environment variables
  */
 const s3Client = new S3Client({
-    region: process.env.NEXT_PUBLIC_AWS_S3_REGION as string,
+    region: process.env.AWS_S3_REGION as string,
     credentials: {
-        accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY_ID as string,
-        secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY as string,
+        accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY as string,
     },
 });
 
@@ -23,7 +23,7 @@ async function uploadFileToS3(file: Buffer, fileName: string, contentType: strin
     const fileBuffer = file;
    
     const params = {
-        Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME as string,
+        Bucket: process.env.AWS_S3_BUCKET_NAME as string,
         Key: `imagesFolder/${fileName}-${Date.now()}`,
         Body: fileBuffer,
         ContentType: contentType,
