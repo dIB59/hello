58e054e698e5186ba5c3a1bd7e91ba4c59245e88
aswka169 <aswka169@student.liu.se>
2024-09-26 16:45:08 +0200

    Adding health check

--- webserver/src/routes.rs
+++ webserver/src/routes.rs
@@ -15,7 +15,7 @@ pub fn init(cfg: &mut web::ServiceConfig) {
             .configure(protect)
             .configure(user_routes)
             .configure(auth_routes)
-            .route("/health", web::get().to(health_check))
+            .configure(health_routes)
             .route("/count", web::get().to(get_count)),
     );
     cfg.service(web::scope("/ws").route("/chat", web::get().to(chat_route)));
@@ -35,4 +35,8 @@ fn protect(cfg: &mut web::ServiceConfig) {
             .wrap(auth_middleware::Auth)
             .service(user_handler::me),
     );
+}
+
+fn health_routes(cfg: &mut web::ServiceConfig) {
+    cfg.route("/health", web::get().to(health_check)); 
 }
