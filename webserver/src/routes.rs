c648ee7fb7edd69a4f48c9240f56deda7b0e203c
aswka169 <aswka169@student.liu.se>
2024-10-04 11:23:41 +0200

    routes

--- webserver/src/routes.rs
+++ webserver/src/routes.rs
@@ -1,42 +1,29 @@
 use actix_web::web;
+
 use task_handler::task_routes;
-use crate::routes::health_handler::health_routes;
+use user_handler::user_routes;
 
 use crate::{
-    auth::auth_middleware,
-    chat::{chat_route, get_count},
+    chat::{chat_routes::chat_route_auth, get_count},
     handlers::*,
 };
+use crate::auth::auth_middleware;
+use crate::chat::chat_handler;
+use crate::handlers::project_handler::project_routes;
+use crate::routes::health_handler::health_routes;
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api/v1")
             .configure(task_routes)
-            .configure(protect)
             .configure(user_routes)
-            .configure(auth_routes)
+            .configure(project_routes)
+            .configure(chat_route_auth)
             .configure(health_routes)
             .route("/count", web::get().to(get_count)),
     );
     cfg.service(
         web::scope("/ws")
             .wrap(auth_middleware::Auth)
-            .route("/chat", web::get().to(chat_route)));
+            .route("/chat", web::get().to(chat_handler)));
 }
-
-fn user_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/users").service(user_handler::register));
-}
-
-fn auth_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/auth").service(auth_handler::login));
-}
-
-fn protect(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::scope("/protected")
-            .wrap(auth_middleware::Auth)
-            .service(user_handler::me),
-    );
-}
-
