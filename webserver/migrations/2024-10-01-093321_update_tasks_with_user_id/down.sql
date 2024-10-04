bf1cb9cbdf71c76ad65351fc66d6d6fd36c81024
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-10-04 10:37:10 +0200

    feat: add index on user_id and project_id

--- webserver/migrations/2024-10-01-093321_update_tasks_with_user_id/down.sql
+++ webserver/migrations/2024-10-01-093321_update_tasks_with_user_id/down.sql
@@ -1,4 +1,7 @@
 -- This file should undo anything in `up.sql`
+DROP INDEX IF EXISTS idx_tasks_user_id;
+DROP INDEX IF EXISTS idx_tasks_project_id;
+
 ALTER TABLE tasks
 DROP COLUMN user_id,
 DROP COLUMN project_id;
