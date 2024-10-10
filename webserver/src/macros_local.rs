cd58cfb4c0030979f6efeaf8fec75b0de83d659e
taza53 <smurtaza.53@outlook.com>
2024-10-10 16:05:11 +0200

    comments explaining macro

--- webserver/src/macros_local.rs
+++ webserver/src/macros_local.rs
@@ -1,3 +1,11 @@
+// let mut conn = pool.get().expect("Failed to get DB connection.");
+// let conn_asyc = web::block(move || {
+//         pool.clone().get().expect("Failed to get DB connection.")
+//     })
+//     .await;
+// let mut conn = conn_asyc.expect("Failed to establish connection");
+// replaces the above code with the following
+// let mut conn = get_db_connection!(pool);
 #[macro_export]
 macro_rules! get_db_connection {
     ($pool:expr) => {{
