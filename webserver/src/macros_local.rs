10e4084187d1c03c602e1ac144dc9cc71a01d9ed
taza53 <smurtaza.53@outlook.com>
2024-10-14 14:52:52 +0200

    update macro, with query async

--- webserver/src/macros_local.rs
+++ webserver/src/macros_local.rs
@@ -2,16 +2,19 @@
 // line above blocking code
 // let conn_asyc = web::block(move || {
 //         pool.clone().get().expect("Failed to get DB connection.")
+//         your variable =  anything 
+//         your_query(&mut conn, anything)
 //     })
 //     .await;
 // let mut conn = conn_asyc.expect("Failed to establish connection");
 // replaces the above code with the following
-// let mut conn = get_db_connection!(pool);
+// let mut conn = get_db_connection_async!(pool);
 #[macro_export]
-macro_rules! get_db_connection {
-    ($pool:expr) => {{
+macro_rules! get_db_connection_async {
+    ($pool:expr,$query:expr) => {{
         web::block(move || {
-            $pool.clone().get().expect("couldn't get db connection from pool")
+            let mut conn = $pool.clone().get().expect("couldn't get db connection from pool");
+            $query(&mut conn)
         })
         .await
         .map_err(actix_web::error::ErrorInternalServerError).expect("internal server error")
