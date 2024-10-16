971ee62fb07aaf25b41eb6cc8ae7a42433cb597c
Ibrahim <Ibrahim@ebs.net.pk>
2024-10-16 20:23:59 +0200

    feat: Add ApiError enum and implement ResponseError trait
     `ApiError` enum and implements the `ResponseError` trait in the `error.rs` file. The `ApiError` enum handles errors related to the database and authorization. It includes variants for `DatabaseApiError` and `AuthorizationError`. The `status_code` and `error_response` methods are implemented to handle the appropriate HTTP status codes and error responses.

---
+++ webserver/src/handlers/error.rs
@@ -0,0 +1,28 @@
+use actix_web::ResponseError;
+use thiserror::Error;
+use crate::auth::error::AuthError;
+use crate::database::error::DatabaseError;
+
+#[derive(Error, Debug)]
+pub enum ApiError {
+    #[error("Database error occurred")]
+    DatabaseApiError(#[from] DatabaseError),
+    #[error("You are not authorized to perform this action")]
+    AuthorizationError(#[from] AuthError),
+}
+
+impl ResponseError for ApiError {
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        match *self {
+            ApiError::DatabaseApiError(ref err) => err.status_code(),
+            ApiError::AuthorizationError(ref err) => err.status_code(),
+        }
+    }
+
+    fn error_response(&self) -> actix_web::HttpResponse {
+        match *self {
+            ApiError::DatabaseApiError(ref err) => err.error_response(),
+            ApiError::AuthorizationError(ref err) => err.error_response(),
+        }
+    }
+}
