00d9c8fb44dffef3952da9f2c7cccea1d4457ff0
Ibrahim <Ibrahim@ebs.net.pk>
2024-10-16 20:23:08 +0200

    feat: Add error module to database module
     The error module contains a DatabaseError enum that represents different types of errors that can occur during database operations.
     It also implements the ResponseError trait from the actix_web crate to handle error responses.
     The DatabaseError enum has variants for generic errors, connection errors, and Diesel errors.

---
+++ webserver/src/database/error.rs
@@ -0,0 +1,40 @@
+use std::fmt::{Display, Formatter};
+use actix_web::{HttpResponse, ResponseError};
+use actix_web::http::StatusCode;
+use diesel::r2d2;
+use thiserror::Error;
+use diesel::result::Error as DieselError;
+
+#[derive(Error, Debug)]
+pub enum DatabaseError {
+    #[error("Database error occurred")]
+    GenericError,
+    #[error("Database connection error occurred")]
+    ConnectionError(#[from] r2d2::PoolError),
+    #[error("Diesel error occurred")]
+    DieselError(#[from] DieselError),
+}
+
+impl ResponseError for DatabaseError {
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            DatabaseError::GenericError => StatusCode::INTERNAL_SERVER_ERROR,
+
+            DatabaseError::ConnectionError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+
+            DatabaseError::DieselError(ref err) => match &err {
+                DieselError::DatabaseError(_, _) => StatusCode::CONFLICT,
+                _ => StatusCode::INTERNAL_SERVER_ERROR,
+            }
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        let status_code = self.status_code();
+        let error_message = self.to_string();
+
+        log::error!("{:?}", self);
+
+        HttpResponse::build(status_code).body(error_message)
+    }
+}
